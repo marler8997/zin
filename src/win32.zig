debd8b91867639b3202cab991c72658d96795c0a
Jonathan Marler <johnnymarler@gmail.com>
2025-08-25 12:41:24 -0600

    x11 key support

--- src/win32.zig
+++ src/win32.zig
@@ -322,15 +322,18 @@ pub const VirtualKey = enum(u16) {
     xbutton2 = @intFromEnum(win32.VK_XBUTTON2),
 
     // Basic keys
-    back = @intFromEnum(win32.VK_BACK),
+    backspace = @intFromEnum(win32.VK_BACK),
     tab = @intFromEnum(win32.VK_TAB),
     clear = @intFromEnum(win32.VK_CLEAR),
     @"return" = @intFromEnum(win32.VK_RETURN),
 
     // Modifier keys
-    shift = @intFromEnum(win32.VK_SHIFT),
-    control = @intFromEnum(win32.VK_CONTROL),
-    alt = @intFromEnum(win32.VK_MENU),
+    // shift_left = @intFromEnum(win32.VK_SHIFT),
+    // shift_right = @intFromEnum(win32.VK_SHIFT),
+    // control_left = @intFromEnum(win32.VK_CONTROL),
+    // control_right = @intFromEnum(win32.VK_CONTROL),
+    // alt_left = @intFromEnum(win32.VK_MENU),
+    // alt_right = @intFromEnum(win32.VK_MENU),
     pause = @intFromEnum(win32.VK_PAUSE),
     caps_lock = @intFromEnum(win32.VK_CAPITAL),
 
@@ -349,7 +352,6 @@ pub const VirtualKey = enum(u16) {
     modechange = @intFromEnum(win32.VK_MODECHANGE),
 
     // Navigation keys
-    space = @intFromEnum(win32.VK_SPACE),
     page_up = @intFromEnum(win32.VK_PRIOR),
     page_down = @intFromEnum(win32.VK_NEXT),
     end = @intFromEnum(win32.VK_END),
@@ -365,44 +367,6 @@ pub const VirtualKey = enum(u16) {
     insert = @intFromEnum(win32.VK_INSERT),
     delete = @intFromEnum(win32.VK_DELETE),
     help = @intFromEnum(win32.VK_HELP),
-
-    @"0" = @intFromEnum(win32.VK_0),
-    @"1" = @intFromEnum(win32.VK_1),
-    @"2" = @intFromEnum(win32.VK_2),
-    @"3" = @intFromEnum(win32.VK_3),
-    @"4" = @intFromEnum(win32.VK_4),
-    @"5" = @intFromEnum(win32.VK_5),
-    @"6" = @intFromEnum(win32.VK_6),
-    @"7" = @intFromEnum(win32.VK_7),
-    @"8" = @intFromEnum(win32.VK_8),
-    @"9" = @intFromEnum(win32.VK_9),
-
-    a = @intFromEnum(win32.VK_A),
-    b = @intFromEnum(win32.VK_B),
-    c = @intFromEnum(win32.VK_C),
-    d = @intFromEnum(win32.VK_D),
-    e = @intFromEnum(win32.VK_E),
-    f = @intFromEnum(win32.VK_F),
-    g = @intFromEnum(win32.VK_G),
-    h = @intFromEnum(win32.VK_H),
-    i = @intFromEnum(win32.VK_I),
-    j = @intFromEnum(win32.VK_J),
-    k = @intFromEnum(win32.VK_K),
-    l = @intFromEnum(win32.VK_L),
-    m = @intFromEnum(win32.VK_M),
-    n = @intFromEnum(win32.VK_N),
-    o = @intFromEnum(win32.VK_O),
-    p = @intFromEnum(win32.VK_P),
-    q = @intFromEnum(win32.VK_Q),
-    r = @intFromEnum(win32.VK_R),
-    s = @intFromEnum(win32.VK_S),
-    t = @intFromEnum(win32.VK_T),
-    u = @intFromEnum(win32.VK_U),
-    v = @intFromEnum(win32.VK_V),
-    w = @intFromEnum(win32.VK_W),
-    x = @intFromEnum(win32.VK_X),
-    y = @intFromEnum(win32.VK_Y),
-    z = @intFromEnum(win32.VK_Z),
 
     left_super = @intFromEnum(win32.VK_LWIN),
     right_super = @intFromEnum(win32.VK_RWIN),
@@ -452,14 +416,14 @@ pub const VirtualKey = enum(u16) {
     f24 = @intFromEnum(win32.VK_F24),
 
     numlock = @intFromEnum(win32.VK_NUMLOCK),
-    scroll = @intFromEnum(win32.VK_SCROLL),
+    scroll_lock = @intFromEnum(win32.VK_SCROLL),
 
-    left_shift = @intFromEnum(win32.VK_LSHIFT),
-    right_shift = @intFromEnum(win32.VK_RSHIFT),
-    left_control = @intFromEnum(win32.VK_LCONTROL),
-    right_control = @intFromEnum(win32.VK_RCONTROL),
-    left_alt = @intFromEnum(win32.VK_LMENU),
-    right_alt = @intFromEnum(win32.VK_RMENU),
+    // left_shift = @intFromEnum(win32.VK_LSHIFT),
+    // right_shift = @intFromEnum(win32.VK_RSHIFT),
+    // left_control = @intFromEnum(win32.VK_LCONTROL),
+    // right_control = @intFromEnum(win32.VK_RCONTROL),
+    // left_alt = @intFromEnum(win32.VK_LMENU),
+    // right_alt = @intFromEnum(win32.VK_RMENU),
 
     browser_back = @intFromEnum(win32.VK_BROWSER_BACK),
     browser_forward = @intFromEnum(win32.VK_BROWSER_FORWARD),
@@ -469,14 +433,14 @@ pub const VirtualKey = enum(u16) {
     browser_favorites = @intFromEnum(win32.VK_BROWSER_FAVORITES),
     browser_home = @intFromEnum(win32.VK_BROWSER_HOME),
 
-    volume_mute = @intFromEnum(win32.VK_VOLUME_MUTE),
-    volume_down = @intFromEnum(win32.VK_VOLUME_DOWN),
-    volume_up = @intFromEnum(win32.VK_VOLUME_UP),
+    // volume_mute = @intFromEnum(win32.VK_VOLUME_MUTE),
+    // volume_down = @intFromEnum(win32.VK_VOLUME_DOWN),
+    // volume_up = @intFromEnum(win32.VK_VOLUME_UP),
 
-    media_next_track = @intFromEnum(win32.VK_MEDIA_NEXT_TRACK),
-    media_prev_track = @intFromEnum(win32.VK_MEDIA_PREV_TRACK),
-    media_stop = @intFromEnum(win32.VK_MEDIA_STOP),
-    media_play_pause = @intFromEnum(win32.VK_MEDIA_PLAY_PAUSE),
+    // media_next_track = @intFromEnum(win32.VK_MEDIA_NEXT_TRACK),
+    // media_prev_track = @intFromEnum(win32.VK_MEDIA_PREV_TRACK),
+    // media_stop = @intFromEnum(win32.VK_MEDIA_STOP),
+    // media_play_pause = @intFromEnum(win32.VK_MEDIA_PLAY_PAUSE),
 
     launch_mail = @intFromEnum(win32.VK_LAUNCH_MAIL),
     launch_media_select = @intFromEnum(win32.VK_LAUNCH_MEDIA_SELECT),
@@ -536,6 +500,7 @@ fn keyFromWin32(direction: enum { up, down }, wparam: win32.WPARAM, lparam: win3
         .vk = @enumFromInt(@as(u16, @intCast(0xffff & wparam))),
         .scan_code = @enumFromInt(key_flags.scan_code),
         .win32_extended = key_flags.extended,
+        .x11_mask = {},
     };
 }
 
